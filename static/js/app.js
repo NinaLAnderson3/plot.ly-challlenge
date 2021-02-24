29fe2b5ee489faeaf2986b9e160a7b9267a4dbac
Nina Anderson <ninamonaco@yahoo.com>
2021-02-23 20:27:47 -0500

    new code

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,45 @@
-//create a function to get data
-var data = d3.select("samples.json");
-console.log(data)
+
+function getData() {
+var dropdown = d3.select("#selDataset");
+
+//read dataset
+d3.json("samples.json").then((data)=>{
+    console.log(data);
+
+    var name = data.names;
+
+name.forEach(function(name) {
+    dropdown.append("option").text(name).property("value");
+});
+
+var firstData = name[0];
+Metadata(firstData);
+})
+};
+
+getData();
+
+// function Metadata(id) {
+//     //read json
+//     d3.json("samples.json").then((data)=> {
+        
+        
+//         var Metadata = data.metadata;
+
+//         console.log(Metadata);
+
+//         var dataArray = Metadata.filter(sampleobject =>sampleobject.id === id)[0];
+//         var result = dataArray;
+//         var display = d3.select("sample-metadata");
+//         display.html("");
+//         Object.entries(result).forEach((key)=>{
+//             display.append("h5").text(`${key}:${value}`);
+        
+//         });
+//     });
+// }
+
+// function optionChange(id){
+//     Metadata(id);
+    
+// }
