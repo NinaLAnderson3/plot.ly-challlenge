c695d76ea7675483e1fb6356de6324ddfbe275f0
Nina Anderson <ninamonaco@yahoo.com>
2021-02-25 18:37:25 -0500

    bar graph code

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-// create the function to get the necessary data
+// create function to get data
 function getInfo(id) {
     // read the json file to get data
     d3.json("samples.json").then((data)=> {
         
-        // get the metadata info for the demographic panel
+        // get metadata info for the demographic panel
         var metadata = data.metadata;
 
         console.log(metadata)
 
-        // filter meta data info by id
+        // filter metadata info by id
         var result = metadata.filter(meta => meta.id.toString() === id)[0];
 
-        // select demographic panel to put data
+        // select demog panel to put data
         var demographicInfo = d3.select("#sample-metadata");
         
         // empty the demographic info panel each time before getting new id info
@@ -26,11 +26,11 @@ function getInfo(id) {
 
 // create the function for the change event
 function optionChanged(id) {
-    // getPlot(id);
+    getPlotData(id);
     getInfo(id);
 }
 
-// create the function for the initial data rendering
+// create the function to render data
 function init() {
     // select dropdown menu 
     var dropdown = d3.select("#selDataset");
@@ -51,3 +51,45 @@ function init() {
 }
 
 init();
+
+function getPlotData(id){
+    d3.json("samples.json").then((data) => {
+        console.log(id)
+
+        var samples = data.samples.filter(samp=>samp.id===id)[0];
+        console.log(samples);
+
+        var topTen = samples.sample_values.slice(0,10).reverse();
+
+        var OTUs = (samples.otu_ids.slice(0,10)).reverse();
+        console.log(OTUs);
+
+        var OTU_ids = OTUs.map(ids=>"OTU"+ids);
+        console.log(OTU_ids);
+
+        var labels = samples.otu_labels.slice(0,10);
+
+        var trace = {
+            x: topTen,
+            y: OTU_ids,
+            text: labels,
+            type: "bar",
+            orientation: "h"
+        };
+
+        var data1 = [trace];
+        console.log(data);
+
+        var layout = {
+            title: "Top 10 OTUs",
+            yaxis: {
+                tickmode: "linear"
+            }
+        };
+        Plotly.newPlot("bar", data1, layout);
+    });
+    
+}
+getPlotData();
+
+
