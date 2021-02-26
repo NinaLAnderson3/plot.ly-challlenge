9e78fb89b37ba7be6f460abc97b4772867762dc5
Nina Anderson <ninamonaco@yahoo.com>
2021-02-26 13:53:08 -0500

    Final commit

--- app.js
+++ app.js
@@ -55,20 +55,20 @@ init();
 function getPlotData(id){
     d3.json("samples.json").then((data) => {
         console.log(id)
-
+//get sample data
         var samples = data.samples.filter(samp=>samp.id===id)[0];
         console.log(samples);
-
+//get values of top 10 sample values
         var topTen = samples.sample_values.slice(0,10).reverse();
-
+//get top 10 sample OTU_ids
         var OTUs = (samples.otu_ids.slice(0,10)).reverse();
         console.log(OTUs);
-
+//put OTU_ids into format for the graph
         var OTU_ids = OTUs.map(ids=>"OTU"+ids);
         console.log(OTU_ids);
-
+//create name variable for the top OTU labels
         var labels = samples.otu_labels.slice(0,10);
-
+//create trace data for bar graph
         var trace = {
             x: topTen,
             y: OTU_ids,
@@ -79,7 +79,7 @@ function getPlotData(id){
 
         var data1 = [trace];
         console.log(data);
-
+//create layout variable for line graph
         var layout = {
             title: "Top 10 OTUs",
             yaxis: {
@@ -87,7 +87,7 @@ function getPlotData(id){
             }
         };
         Plotly.newPlot("bar", data1, layout);
-
+//create trace of data for bubble graph
         var trace1 = {
             x: samples.otu_ids,
             y: samples.sample_values,
@@ -97,6 +97,8 @@ function getPlotData(id){
                 color: samples.otu_ids
             }
         };
+
+        //create layout variable for bubble graph
         var layout2 = {
             xaxis:{title: "OTU ID"},
             height: 500,
@@ -106,6 +108,24 @@ function getPlotData(id){
         var data1 = [trace1];
     
         Plotly.newPlot("bubble", data1, layout2)
+
+        //washing frequency gauge data
+
+        d3.json("samples.json").then((data)=>{
+            var washingFreq = data.metadata.map(d=>d.wfreq)
+            console.log(washingFreq);
+
+            var gaugeData = [
+                {domain: {x:[0,1], y:[0,1]},
+                value: parseFloat(washingFreq),
+                title: {text: "Washing Frequency"},
+                type: "indicator",
+                mode: "gauge + number"
+            }
+            ];
+            var gaugeLayout = {width: 300, height:200, margin:{t:0, b:0}};
+            Plotly.newPlot('.gauge',washingFreq, gaugeLayout);
+        });
 
     });
     
